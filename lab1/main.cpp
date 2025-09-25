87c6d6a1861f453c27814de589d35e8069782c85
Nikolay Didenko <168210918+KateSnvn@users.noreply.github.com>
2025-09-25 19:27:25 +0200

    make lab1

---
+++ lab1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+//3 вариант
+
+unsigned short num1; 
+unsigned short num2;
+/*
+По условию нужно найти сумму, разность, произведение 2х неотрицательных чисел. 
+Выбрала тип данных unsigned short.
+Так как unsigned указывает, что переменная может хранить только неотрицательные значения.
+short для оптимизации кода, так как short по объему памяти заминает меньшее количество байт
+*/
+
+
+short size_short = sizeof(short) * 8;
+short min_short = -pow(2, 15);
+short max_short = pow(2, 15) - 1;
+
+
+int main() {
+    cout << "short занимает " << size_short << " бит, минимальное значение = " << min_short << ", максимальное значение = " << max_short << endl;
+    
+    
+    cin >> num1;
+    cin >> num2;
+
+
+    unsigned short res1 = num1 + num2;
+    short res2 = num1 - num2;
+    /*
+    Для разности выбрала тип данных short (без unsigned), так как есть вероятность получить отрицательное значение (num1 < num2)
+    */
+    unsigned short res3 = num1 * num2;
+
+
+    cout << "Сумма: " << res1 << endl;
+    cout << "Разность: " << res2 << endl;
+    cout << "Произведение: " << res3 << endl;
+
+}
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+*/
+
+#include <iostream>
+int main() {
+
+    int x;
+    int y;
+    std::cin >> x >> y;
+    std::cout << x << " " << y;
+
+    return 0;
+}
