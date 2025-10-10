207a3f2b57eba5f078c732968de673e19ac64c4f
Nikolay Didenko <168210918+KateSnvn@users.noreply.github.com>
2025-10-10 12:10:20 +0200

    complete lab3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -7,77 +7,57 @@ using namespace std;
 float summa = 0;
 bool flag = false;
 float max_num = -1;
-short max_index;
+short max_index = -1;
 
 
 short res = 1;
-short x;
+short x = 0;
 
 int main(){
 
     //------------------------------------------------------------------------- задача 1
-    short N;
-    cin >> N;
+    // short N;
+    // cin >> N;
 
-    for (short i = 0; i < N; i++){
-        float num;
-        cin >> num;
-        if ((num > -0.36) && (num <= 10.1)){
-            flag = true;
-            if (num > max_num){
-                max_num = num;
-                max_index = i + 1;
-            }
-            summa += num;
-        }
-    }
-    if (flag == false){
-            cout << "В последовательности нет чисел из интервала (-0.36; 10.1]";
-        }
-    else {
-        cout << "Сумма всех чисел = " << summa << endl;
-        cout << "Наибольшее число = " << max_num << endl;
-        cout << "Номер в последовательности наибольшего числа = " << max_index;
-    }
+    // for (short i = 0; i < N; i++){
+    //     float num;
+    //     cin >> num;
+    //     if ((num > -0.36) && (num <= 10.1)){
+    //         flag = true;
+    //         if (num > max_num){
+    //             max_num = num;
+    //             max_index = i + 1;
+    //         }
+    //         summa += num;
+    //     }
+    // }
+    // if (flag == false){
+    //         cout << "В последовательности нет чисел из интервала (-0.36; 10.1]";
+    //     }
+    // else {
+    //     cout << "Сумма всех чисел = " << summa << endl;
+    //     cout << "Наибольшее число = " << max_num << endl;
+    //     cout << "Номер в последовательности наибольшего числа = " << max_index;
+    // }
     
 
     //------------------------------------------------------------------------- задача 2
-    // // cin >> x;
+    
 
-    // // if (x == 0){
-    // //     res = 0;
-    // // }
+    cin >> x;
 
-    // // else if (x < 0){
-    // //     x = abs(x);
-    // //     while (x != 0){
-    // //         res *= (x % 10);
-    // //         x /= 10;
-    // //     }
-    // // }
+    if (x == 0){
+        res = 0;
+    }
 
-    // // else if (x > 0){
-    // //     while (x != 0){
-    // //         res *= (x % 10);
-    // //         x /= 10;
-    // //     }
-    // // }
-    // // cout << "Произведение цифр: " << res;
+    else if (abs(x) < 1000){
+        while (abs(x) != 0){
+            res *= (abs(x) % 10);
+            x = (abs(x) / 10);
+        }
+    }
 
-    // cin >> x;
-
-    // if (x == 0){
-    //     res = 0;
-    // }
-
-    // else if (abs(x) < 1000){
-    //     while (abs(x) != 0){
-    //         res *= (abs(x) % 10);
-    //         x = (abs(x) / 10);
-    //     }
-    // }
-
-    // cout << "Произведение цифр: " << res;
+    cout << "Произведение цифр: " << res;
 
     return 0;
 }
