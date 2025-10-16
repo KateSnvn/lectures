421786660ebd58acd1575f3b2593ef1113b16fee
Nikolay Didenko <168210918+KateSnvn@users.noreply.github.com>
2025-10-16 23:15:26 +0200

    make lab4

---
+++ lab4/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//3 вариант 
+
+int main(){
+
+//---------------------------------------------------------------------------------------- задача 1
+
+
+    // int nums[6];
+    // for (int i = 0; i < 6; ++i){
+    //     cin >> nums[i];
+    // }
+
+
+    // bool same = false;
+    // for (int i = 0; i < 6; i++){
+    //     if (nums[i] == nums[i + 1]){
+    //         same = true;
+    //     }
+    // }
+
+
+    // if (same){
+    //     for (int i = 0; i < 6; i++){
+    //         for (int j = i + 1; j < 6; j++){
+    //             if(nums[i] > nums[j]){
+    //                 swap(nums[i], nums[j]);
+    //             }
+    //         }
+    //     }
+    //     for (int i = 0; i < 6; i++){
+    //         cout << nums[i] << " ";
+    //     }
+    // }
+
+    // else{
+    //     for (int i = 0; i < 6; i++){
+    //         cout << nums[i] << " ";
+    //     }
+    // }
+
+
+//---------------------------------------------------------------------------------------- задача 2
+
+    int rows = 3;
+    int cols = 4;
+    int matrix[rows][cols];
+    int max_sum = -1;
+    int max_index = -1;
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++){
+        int summa = 0;
+        for (int j = 0; j < cols; j++){
+            summa += abs(matrix[i][j]);
+        }
+        max_sum = max(summa, max_sum);
+        max_index = max(i, max_index);
+    }
+
+    for (int j = 0; j < cols; j++){
+        matrix[max_index][j] = 0;
+    }
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
