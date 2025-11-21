9fa39722bfb3bc3ef57bf981120420573880ba5f
Nikolay Didenko <168210918+KateSnvn@users.noreply.github.com>
2025-11-21 11:46:53 +0200

    complete lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 //--------------------------------------------------------------------------------------------------задание 1 
 
+
 int* find_zero(int** matrix, int rows, int cols, int& zero_cnt) {
     int* has_zero = (int*)calloc(cols, sizeof(int));
     zero_cnt = 0;
@@ -32,42 +33,6 @@ int* find_zero(int** matrix, int rows, int cols, int& zero_cnt) {
     return zero_cols;
 }
 
-int** remove_cols(int** matrix, int rows, int& cols, int* zero_cols, int zero_cnt) {
-    if (zero_cnt == 0) {
-        return matrix;
-    }
-    
-    int new_cols = cols - zero_cnt;
-    
-    int** new_matrix = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++) {
-        new_matrix[i] = (int*)malloc(new_cols * sizeof(int));
-        
-        int new_j = 0;
-        for (int j = 0; j < cols; j++) {
-            bool skip = false;
-            for (int k = 0; k < zero_cnt; k++) {
-                if (j == zero_cols[k]) {
-                    skip = true;
-                    break;
-                }
-            }
-            
-            if (!skip) {
-                new_matrix[i][new_j++] = matrix[i][j];
-            }
-        }
-    }
-    
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-    cols = new_cols; 
-
-    return new_matrix;
-}
-
 void print_matrix(int** matrix, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -77,36 +42,7 @@ void print_matrix(int** matrix, int rows, int cols) {
     }
 }
 
-//--------------------------------------------------------------------------------------------------задание 2
-/*
-void pointers() {
-        float a, b;
-        cin >> a;
-        cin >> b;
-
-        float* p_a = new float(a);
-        float* p_b = new float(b);
-
-        *p_a *= 3;
-
-        float num_a = *p_a;
-        *p_a = *p_b;
-        *p_b = num_a;
-
-        a = *p_a;
-        b = *p_b;
-
-        cout << "A = " << a << ", B = " << b << endl;
-        
-        delete p_a;
-        delete p_b;
-    }
-*/
-//-----------------------------------------------------------------------------------------------------------
-
-
 int main() {
-
     int rows = 2;
     int cols = 2;
     int** matrix = (int**)malloc(rows * sizeof(int*));
@@ -135,52 +71,111 @@ int main() {
     matrix[0][1] = b;
     matrix[1][0] = c;
     matrix[1][1] = d;
+    
     int new_rows = 2 + a;
     int new_cols = 2 + b;
 
-    int** new_matrix = (int**)calloc(new_rows, sizeof(int*));
+    matrix = (int**)realloc(matrix, new_rows * sizeof(int*));
     for(int i = 0; i < new_rows; i++) {
-        new_matrix[i] = (int*)calloc(new_cols, sizeof(int));
+        if(i < rows) {
+            matrix[i] = (int*)realloc(matrix[i], new_cols * sizeof(int));
+            for(int j = cols; j < new_cols; j++) {
+                matrix[i][j] = 0;
+            }
+        } else {
+            matrix[i] = (int*)calloc(new_cols, sizeof(int));
+        }
     }
 
-    for(int i = 0; i < new_rows; i++) {
-        for(int j = 0; j < new_cols; j++) {
+    rows = new_rows;
+    cols = new_cols;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
             if((i < 2) && (j < 2)) {
-                new_matrix[i][j] = matrix[i][j];
+                continue;
             }
             else {
-                new_matrix[i][j] = (((i - 1) * c) + ((j - 1) * d));
+                matrix[i][j] = (((i - 1) * c) + ((j - 1) * d));
             }
         }
     }
 
     int zero_cnt;
-    int* zero_cols = find_zero(new_matrix, new_rows, new_cols, zero_cnt);
+    int* zero_cols = find_zero(matrix, rows, cols, zero_cnt);
 
-    int current_cols = new_cols;
-    int** final_matrix = remove_cols(new_matrix, new_rows, current_cols, zero_cols, zero_cnt);
+    if (zero_cnt > 0) {
+        int new_cols_count = cols - zero_cnt;
+        
+        int** temp_rows = (int**)malloc(rows * sizeof(int*));
+        
+        for (int i = 0; i < rows; i++) {
+            temp_rows[i] = (int*)malloc(new_cols_count * sizeof(int));
+        
+            int new_j = 0;
+            for (int j = 0; j < cols; j++) {
+                bool skip = false;
+                for (int k = 0; k < zero_cnt; k++) {
+                    if (j == zero_cols[k]) {
+                        skip = true;
+                        break;
+                    }
+                }
+                
+                if (!skip) {
+                    temp_rows[i][new_j++] = matrix[i][j];
+                }
+            }
+            
+            free(matrix[i]);
+        }
+        
+        free(matrix);
+        
+        matrix = temp_rows;
+        cols = new_cols_count;
+    }
 
-    print_matrix(final_matrix, new_rows, current_cols);
+    print_matrix(matrix, rows, cols);
+
+    free(zero_cols);
 
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
 
-    free(zero_cols);
+    return 0;
+}
 
-    for (int i = 0; i < new_rows; i++) {
-        free(final_matrix[i]);
+//--------------------------------------------------------------------------------------------------задание 2
+/*
+void pointers() {
+        float a, b;
+        cin >> a;
+        cin >> b;
+
+        float* p_a = new float(a);
+        float* p_b = new float(b);
+
+        *p_a *= 3;
+
+        float num_a = *p_a;
+        *p_a = *p_b;
+        *p_b = num_a;
+
+        a = *p_a;
+        b = *p_b;
+
+        cout << "A = " << a << ", B = " << b << endl;
+        
+        delete p_a;
+        delete p_b;
     }
-    free(final_matrix);
-
-
-
-    //--------------------------------------------------------------------------------------------------задание 2
+*/
+//-----------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------задание 2
     /*
     pointers();
     */
     //------------------------------------------------------------------------------------------------------------
-
-    return 0;
-}
